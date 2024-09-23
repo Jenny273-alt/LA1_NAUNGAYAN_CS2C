2929d15faf1266c22b2f771ad23fa87833514930
Jenny273-alt <naungayanjenny90@gmail.com>
2024-09-23 19:23:26 +0700

    string.js

---
+++ LA1_NAUNGAYAN_CS2C/string.js
@@ -0,0 +1,12 @@
+let favActorFirstName = "Will";
+let favActorLastName = "Smith";
+let fullName = favActorFirstName + " " + favActorLastName; //contatenate first and last names
+let uppercase = fullName.toUpperCase(); //capitalize the full name
+let message = "My favorite actor is" + uppercase; 
+message += "and his best show is I am legend.";
+
+console.log("First name:", favActorFirstName);
+console.log("Last name:", favActorLastName);
+console.log("Full name:", fullName);
+console.log("Uppercase name:", uppercase);
+console.log("Message:", message);
