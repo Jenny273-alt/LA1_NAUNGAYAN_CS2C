bb4a3f7b4de5372b4eb8c40d69f3048e9f48c18d
Jenny273-alt <naungayanjenny90@gmail.com>
2024-09-23 19:20:03 +0700

    Create number.js

---
+++ LA1_NAUNGAYAN_CS2C/number.js
@@ -0,0 +1,15 @@
+let firstFavNum = 23; //my favorite number
+let secondFavNum = 20; //my second favorite number
+let addition = firstFavNum + secondFavNum; 
+let subtraction = firstFavNum - secondFavNum;
+let multiplication = firstFavNum * secondFavNum;
+let division = firstFavNum / secondFavNum;
+let modulus = firstFavNum % secondFavNum;
+let exponentiation = firstFavNum ** secondFavNum;
+
+console.log("Addition:", addition);
+console.log("Subtraction:", subtraction);
+console.log("Multiplication:", multiplication);
+console.log("Division:", division);
+console.log("Modulus:", modulus);
+console.log("Exponentiation:", exponentiation);
