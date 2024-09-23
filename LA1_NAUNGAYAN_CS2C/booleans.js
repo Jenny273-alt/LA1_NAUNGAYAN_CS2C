0b4fe1caafc7b9c5d043fdabb9b0261ec6445f02
Jenny273-alt <naungayanjenny90@gmail.com>
2024-09-23 19:21:07 +0700

    Create booleans.js

---
+++ LA1_NAUNGAYAN_CS2C/booleans.js
@@ -0,0 +1,9 @@
+let isJsProgrammingLanguage = true; //Is JavaScript a programming language?
+let isJsHard = false; //Is JavaScript hard?
+let favNumb = 23; //my favorite number
+let result = favNumb + undefined; //add fav number to undefined
+
+console.log("isJsProgrammingLanguage:", isJsProgrammingLanguage);
+console.log("isJsHard:", isJsHard);
+console.log("favNumb:", favNumb);
+console.log("Result of favNumb + undefined:", result);
