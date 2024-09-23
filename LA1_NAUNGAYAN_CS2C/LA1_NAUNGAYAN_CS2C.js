2f3dbe9ec073fded1a1de8a111eafa17cc4c3fa0
Jenny273-alt <naungayanjenny90@gmail.com>
2024-09-23 19:17:14 +0700

    Add files via upload

---
+++ LA1_NAUNGAYAN_CS2C/LA1_NAUNGAYAN_CS2C.js
@@ -0,0 +1,9 @@
+let name = "Jenny Naungayan"; //I stored my name
+let whatDoYouWannaBecomeInYourLife = "programmer"; //I stored my dream job
+let gender = "Female"; //I stored my gender
+let facebookAcct = "Jenny Naungayan"; //I stored my facebook account
+
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(facebookAcct);
